2c5368a9c66d1888ebb1b9b5908bb59a6295258f
Darin <e0544532@u.nus.edu>
2022-01-19 20:04:43 +0800

    First commit Week 2 Level-1 Complete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,28 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo + "\n what is your command?");
+
+        while (true) {
+            Scanner scn = new Scanner(System.in);
+            String cmd = scn.nextLine();
+            findCmd(cmd);
+        }
+    }
+
+    private static void findCmd(String cmd) {
+        switch(cmd) {
+            case "list" :
+                System.out.println("list");
+                break;
+            case "blah" :
+                System.out.println("blah");
+                break;
+            case "bye" :
+                System.out.println("Bye. See you later! ");
+                System.exit(0);
+            default:
+                System.out.println("Invalid Command! ");
+        }
     }
 }
