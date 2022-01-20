9fa018e46bdea9d2ae99e311bd5b78785662989e
Darin <e0544532@u.nus.edu>
2022-01-20 12:33:58 +0800

    Level-4 revised

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -9,6 +9,6 @@ public class Event extends Task{
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (on: " + on + ")";
+        return "[E]" + super.toString() + " (" + on + ")";
     }
 }
