958f0fcf6500d14740c68b2d0f235793e25d1c3c
talkintomato <darinloh@gmail.com>
2022-02-03 13:52:30 +0800

    first commit

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -2,7 +2,13 @@ package duke;
 
 import duke.exception.DukeException;
 
-public class Duke {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Duke extends Application {
 
     private TaskList taskList;
 
@@ -10,24 +16,34 @@ public class Duke {
 
     private Ui ui;
 
-    public static void main(String[] args) {
-        new Duke("./src/data/duke.txt").run();
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
+
+
+//    public static void main(String[] args) {
+//        new Duke("./src/data/duke.txt").run();
+//    }
 
     /**
      * Constructor for class
      * @param filePath The path which to save and load taskList state from
      */
-    public Duke(String filePath) {
-        ui = new Ui();
-        storage = new Storage(filePath);
-        try {
-            taskList = new TaskList(storage.load());
-        } catch (Exception e) {
-//            ui.showLoadingError();
-            this.taskList = new TaskList();
-        }
-    }
+//    public Duke(String filePath) {
+//        ui = new Ui();
+//        storage = new Storage(filePath);
+//        try {
+//            taskList = new TaskList(storage.load());
+//        } catch (Exception e) {
+////            ui.showLoadingError();
+//            this.taskList = new TaskList();
+//        }
+//    }
 
     /**
      *  Starts running Duke
