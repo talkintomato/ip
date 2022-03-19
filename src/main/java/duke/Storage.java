7636e93b8fb2fb5b68d43b6e260d874c05414238
talkintomato <darinloh@gmail.com>
2022-03-20 00:50:41 +0800

    saved fixed

--- src/main/java/duke/Storage.java
+++ src/main/java/duke/Storage.java
@@ -1,9 +1,9 @@
 package duke;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -13,10 +13,8 @@ import duke.tasks.*;
 //Solution below adapted from https://github.com/lamwj98/ip/blob/master/src/main/java/siri/Storage.java
 
 public class Storage {
-    private File directory;
-    private File dataFile;
+
     protected ArrayList<Task> todos = new ArrayList<>();
-    private String fileDirectoryPath;
     private String filePath;
 
 
@@ -28,15 +26,8 @@ public class Storage {
      * If empty String is provided, cwd will create a data folder and a data.txt file inside to be saved.
      */
     public Storage(String filePath) {
-
-        if (filePath == "") {
-            this.filePath = System.getProperty("user.home") + "/dukeData/data.txt";
-        } else {
-            this.filePath = filePath;
-        }
-        this.fileDirectoryPath = this.filePath.substring(0, filePath.lastIndexOf('/'));
-        this.directory = new File(fileDirectoryPath);
-        this.dataFile = new File(filePath);
+        assert(filePath != null && !filePath.equals(""));
+        this.filePath = filePath;
     }
 
     /**
@@ -47,32 +38,31 @@ public class Storage {
      */
     public ArrayList<Task> load() throws DukeException {
 
-
-        if (dataFile.exists() == false) {
-            throw new DukeException("File does not exist!");
-        }
-
-        if (directory.exists() == false) {
-            directory.mkdir();
-            throw new DukeException("Directory and file does not exist!");
-        }
-
         try {
-            Scanner sc = new Scanner(dataFile);
+            File directory = new File("data");
+            if (directory.mkdirs()) {
+                System.out.println("Created new directory /data");
+            }
+            File taskTextFile = new File("data/task.txt");
+            if (taskTextFile.createNewFile()) {
+                System.out.println("Created text file as /data/task.txt");
+            }
+            Scanner sc = new Scanner(Paths.get("data/task.txt")); // create a Scanner using the File as the source
 
-            while (sc.hasNextLine()) {
+            ArrayList<Task> todos = new ArrayList<Task>();
+
+            while (sc.hasNext()) {
                 String data = sc.nextLine();
                 String[] taskData = data.split(" , ");
                 Task task = Task.createTask(taskData);
                 todos.add(task);
             }
-
-            sc.close();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
+            return todos;
+        } catch (IOException e) {
+            throw new DukeException(e.getMessage());
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new DukeException(e.getMessage());
         }
-
-        return todos;
 
     }
 
@@ -81,18 +71,9 @@ public class Storage {
      */
     public void saveList(ArrayList<Task> todoNew) {
         todos = todoNew;
-        try {
-            if (dataFile.exists() == true) {
-                dataFile.delete();
-            }
-            dataFile.createNewFile();
-        } catch (IOException ioe) {
-            System.out.println("Error creating data file!");
-            System.out.println(ioe.getMessage());
-        }
 
         try {
-            FileWriter myWriter = new FileWriter(dataFile);
+            FileWriter myWriter = new FileWriter("data/task.txt");
             for (int i = 0; i < todos.size(); i = i + 1) {
                 Task currTask = todos.get(i);
                 if (currTask instanceof Todo) {
